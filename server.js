bc319c148bda158107fbfe9332b37484314dcff8
Patrick Mainka <patrickmainka@gmail.com>
2018-05-02 19:46:56 +0200

    path to views changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
-var fileServer = new(nodeStatic.Server)('/home/pitma/html/webmeet');
+var fileServer = new(nodeStatic.Server)('/views');
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(65396,function(){
